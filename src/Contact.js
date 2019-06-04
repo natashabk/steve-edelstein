f0b03214e5fd9b7831c7c9ccc4dff9cdd0046e48
natashabuck <natashabuck1@gmail.com>
2019-06-04 08:49:23 -0700

    form

--- src/Contact.js
+++ src/Contact.js
@@ -2,28 +2,31 @@ import React, { Component } from "react";
 // import { Tabs, Tab } from "react-bootstrap";
 
 export default class Contact extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-  }
-  
   render() {
     return (
       <div className="Home">
         <h3 className="about_heading_top">Getting in Touch</h3>
-        <form name="contact" netlify onSubmit={this.handleSubmit}>
-  <p>
-    <label>Your Name<input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email address<input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Subject<input type="text" name="subject" /></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     );
   }

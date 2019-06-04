1b6a1946b8b06f850aabc55c328719df1aa86319
natashabuck <natashabuck1@gmail.com>
2019-06-04 09:50:34 -0700

    styling on forms page

--- src/Contact.js
+++ src/Contact.js
@@ -1,42 +1,61 @@
 import React, { Component } from "react";
-import NetlifyForm from 'react-netlify-form'
+import NetlifyForm from "react-netlify-form";
+import { Row, Col } from "react-bootstrap";
 
 export default class Contact extends Component {
   render() {
     return (
       <div className="Home">
         <h3 className="about_heading_top">Getting in Touch</h3>
-        <NetlifyForm name="contact">
-        {({ loading, error, success }) => (
-          <div>
-          {loading &&
-            <div>Loading...</div>
-          }
-          {error &&
-            <div>Your information was not sent. Please try again later.</div>
-          }
-          {success &&
-            <div>Thank you for contacting us!</div>
-          }
-          {!loading && !success &&
-            <div>
-             <label>Your name</label>
-             <input type="text" name="subject"  value="New Inquiry from Edelstein Photography!" hidden/>
-              <input type='text' name='name' required />
-              <br/>
-              <label>Email</label>
-              <input type='email' name="email" required />
-              <br/>
-              <textarea name='message' placeholder="Enter your message" required />
-              <br/>
-              <button>Submit</button>
-            </div>
-          }
+        <div id="formWrapper">
+          <Row>
+            <Col>
+            <NetlifyForm name="contact">
+              {({ loading, error, success }) => (
+                <div>
+                  {loading && <div>Loading...</div>}
+                  {error && (
+                    <div>
+                      Your information was not sent. Please try again later.
+                    </div>
+                  )}
+                  {success && <div>Thank you for contacting us!</div>}
+                  {!loading && !success && (
+                    <div>
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Your name"
+                        required
+                      />
+                      <input
+                        type="text"
+                        name="subject"
+                        value="New Inquiry from Edelstein Photography!"
+                        hidden
+                      />
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                        required
+                      />
+                      <textarea
+                        name="message"
+                        placeholder="Enter your message"
+                        required
+                        rows={4}
+                      />
+                      <button id="submit">SUBMIT</button>
+                    </div>
+                  )}
+                </div>
+              )}
+            </NetlifyForm>
+            </Col>
+            <Col></Col>
+          </Row>
         </div>
-      )}
-
-</NetlifyForm>
-
       </div>
     );
   }

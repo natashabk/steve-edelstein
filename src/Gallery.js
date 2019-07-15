e875d47ce60148571a05194833709c8f7f9c8481
Natasha Buck <natasha@Natashas-MacBook-Pro.local>
2019-07-15 08:48:58 +0100

    still messing around with scaling

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,84 +4,86 @@ import imgLinks from "./Modules/ImgLinks";
 import captions from "./Modules/ImgCaptions";
 
 export default class Gallery extends Component {
-  state = {
-    active: 0,
-    captions: captions(),
-    brokenImgs: []
-  };
+	state = {
+		active: 0,
+		captions: captions(),
+		brokenImgs: [],
+	};
 
-  handleClick = e => {
-    const selection = typeof e !== "number" ? Number(e.target.id) : e;
-    this.setState({
-      active: selection
-    });
-  };
+	handleClick = e => {
+		const selection = typeof e !== "number" ? Number(e.target.id) : e;
+		this.setState({
+			active: selection,
+		});
+	};
 
-  getCarouselItems() {
-    return imgLinks[this.props.tab].map(img => {
-      if (!this.state.brokenImgs.includes(img)) {
-        return (
-          <CarouselItem key={img}>
-            <img className="gallery_carousel" src={img} alt={`${img}-lg`} />
-            <Carousel.Caption>
-              <h2 id="textCaption">{this.state.captions.streetsampler[img.slice(67,-4)]}</h2>
-            </Carousel.Caption>
-          </CarouselItem>
-        );
-      } else return null;
-    });
-  }
+	getCarouselItems() {
+		return imgLinks[this.props.tab].map(img => {
+			if (!this.state.brokenImgs.includes(img)) {
+				return (
+					<CarouselItem key={img}>
+						<img className="gallery_carousel" src={img} alt={`${img}-lg`} />
+						<Carousel.Caption>
+							<h2 id="textCaption">
+								{this.state.captions.streetsampler[img.slice(67, -4)]}
+							</h2>
+						</Carousel.Caption>
+					</CarouselItem>
+				);
+			} else return null;
+		});
+	}
 
-  getGalleryItems(start, end) {
-    return imgLinks[this.props.tab].slice(start, end).map(img => {
-      if (!this.state.brokenImgs.includes(img)) {
-        return (
-          <img
-            key={img}
-            className="gallery_img"
-            src={img}
-            id={imgLinks[this.props.tab].indexOf(img)}
-            onClick={this.handleClick}
-            alt={`${img}-thumb`}
-            onError={() =>
-              this.setState({
-                brokenImgs: this.state.brokenImgs.concat(img)
-              })
-            }
-          />
-        );
-      } else return null;
-    });
-  }
+	getGalleryItems(start, end) {
+		return imgLinks[this.props.tab].slice(start, end).map(img => {
+			if (!this.state.brokenImgs.includes(img)) {
+				return (
+					<img
+						key={img}
+						className="gallery_img"
+						src={img}
+						id={imgLinks[this.props.tab].indexOf(img)}
+						onClick={this.handleClick}
+						alt={`${img}-thumb`}
+						onError={() =>
+							this.setState({
+								brokenImgs: this.state.brokenImgs.concat(img),
+							})
+						}
+					/>
+				);
+			} else return null;
+		});
+	}
 
-  render() {
-    return (
-      <div className="gallery">
-        <Row className="gallery_row" noGutters={true}>
-          <Col className="gallery_wrap_col">
-            <Col xs={12} className="gallery_col">
-              {this.getGalleryItems(0, 8)}
-            </Col>
-            <Col xs={12} className="gallery_col">
-              {this.getGalleryItems(8, 16)}
-            </Col>
-            <Col xs={12} className="gallery_col">
-              {this.getGalleryItems(16, 24)}
-            </Col>
-          </Col>
+	render() {
+		return (
+			<div className="gallery">
+				<Row className="gallery_row" noGutters={true}>
+					<Col className="gallery_wrap_col" md={12} lg={4}>
+						<Col xs={12} className="gallery_col">
+							{this.getGalleryItems(0, 8)}
+						</Col>
+						<Col xs={12} className="gallery_col">
+							{this.getGalleryItems(8, 16)}
+						</Col>
+						<Col xs={12} className="gallery_col">
+							{this.getGalleryItems(16, 24)}
+						</Col>
+					</Col>
 
-          <Col sm={12} className="gallery_carousel">
-            <Carousel
-              slide={false}
-              activeIndex={this.state.active}
-              onSelect={this.handleClick}
-              interval={null}
-            >
-              {this.getCarouselItems()}
-            </Carousel>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+					<Col className="gallery_carousel" md={12} lg={8}>
+						<Carousel
+							slide={false}
+							activeIndex={this.state.active}
+							onSelect={this.handleClick}
+							interval={null}
+						>
+							{this.getCarouselItems()}
+						</Carousel>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import { Carousel, CarouselItem} from "react-bootstrap";
+import { Carousel, CarouselItem } from "react-bootstrap";
 import imgLinks from "./Modules/ImgLinks";
 
 export default class Home extends Component {
-  getCarouselItems() {
-    return imgLinks.featured.map(
-      img => (
-        <CarouselItem key={img}>
-          <img className="gallery_carousel_featured" src={img} alt={`${img}-feat`} />
-        </CarouselItem>
-      )
-    );
-  }
-  render() {
-    return <Carousel>{this.getCarouselItems()}</Carousel>;
-  }
+	getCarouselItems() {
+		return imgLinks.featured.map(img => (
+			<CarouselItem key={img}>
+				<img
+					className="gallery_carousel_featured"
+					src={img}
+					alt={`${img}-feat`}
+				/>
+			</CarouselItem>
+		));
+	}
+	render() {
+		return (
+			<div className="homeWrapper">
+				<Carousel>{this.getCarouselItems()}</Carousel>
+			</div>
+		);
+	}
 }
